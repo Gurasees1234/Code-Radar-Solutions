4e30dcd1a393a08665e5ea1acd2bd48877b7b994
Gurasees Kaur <180003228+Gurasees1234@users.noreply.github.com>
2025-01-30 10:06:43 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    ch1=toupper(ch)
+    switch(){
+        case "A":case "E":case "I":case "O":case "U"{
+            printf("Vowel")
+        }
+        
+    }
+    printf("%s", welcome());
+    return 0;
+}
