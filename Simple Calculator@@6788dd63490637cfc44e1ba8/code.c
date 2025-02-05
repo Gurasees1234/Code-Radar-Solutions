5d518fbf7a2ba2c5365d00bf1831806a8b02e118
Gurasees Kaur <180003228+Gurasees1234@users.noreply.github.com>
2025-02-05 11:55:24 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -9,7 +9,7 @@ int main() {
     else if(c== '*'){printf("%d",a*b);}
     else if(c== '/'){
         if(b!=0){printf("%d",a/b);}
-        else{printf("Error");}}
-    else{printf("Error");}
+        else{printf("error");}}
+    else{printf("error");}
     return 0;
 }
